fee676b141d703112ab88a55f1ff39bea1b6da41
Electr0n <skorodumov.eugene@gmail.com>
2017-09-05 15:32:52 +0300

    plugin version for isse/time_entries query

--- plugins/redmine_issue_version_filter/lib/redmine_issue_version_filter/issue_version_filter_query_patch.rb
+++ plugins/redmine_issue_version_filter/lib/redmine_issue_version_filter/issue_version_filter_query_patch.rb
@@ -40,11 +40,20 @@ module IssueVersionFilterQueryPatch
 
       version_ids = scope.open.visible.all().collect(&:id).push(0)
       iss_ids = Issue.where(("fixed_version_id " + operator + " ? "), value).collect(&:id)
-    
-      if iss_ids.empty?
-        "(#{Issue.table_name}.id IS NULL)"
-      else
-        "(#{Issue.table_name}.id IN (#{iss_ids.join(',')}))"
+
+      case self.type
+      when 'IssueQuery'
+        if iss_ids.empty?
+          "(#{Issue.table_name}.id IS NULL)"
+        else
+          "(#{Issue.table_name}.id IN (#{iss_ids.join(',')}))"
+        end
+      when 'TimeEntryQuery'
+        if iss_ids.empty?
+          "(#{TimeEntry.table_name}.issue_id IS NULL)"
+        else
+          "(#{TimeEntry.table_name}.issue_id IN (#{iss_ids.join(',')}))"
+        end
       end
 
     end
